311c09b073de81457bacea658697fbbcd0cfe9ca
Ali Jafari <123054336+alimuradjafari@users.noreply.github.com>
2026-03-24 03:47:51 -0700

    country added in user input

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ print('we have learned the commit commmand in this stage')
 
 name = input("Enter the your name: ")
 age =input('Enter your age: ')
+country = input("Enter your country: ")
 
-print(f'Hi, I am {name}, {age} years old.')
+print(f'Hi, I am {name}, {age} years old from {country}.')
