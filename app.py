52551552aa00c5050a61a0d240b8d33b03d83e36
alimuradjafari <alimuradjafari@gmail.com>
2026-03-24 03:21:52 -0700

    getting user input

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 print('hello world')
 print('i am learning git and github')
 print('we have learned the commit commmand in this stage')
+
+name = input("Enter the your name: ")
+age =input('Enter your age: ')
+
+print(f'Hi, I am {name}, {age} years old.')
